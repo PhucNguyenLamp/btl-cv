cd13bcd29b80fbd54f171125df9a39823754e14b
Lamp <giaphuc29082004@gmail.com>
2025-10-27 19:40:34 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# algorithm, không dùng thư viện
+
+# load 2 ảnh
+# pick điểm tương ứng trên 2 ảnh
+# gen delaunay triangle
+# tìm abcdef
+# warp từng triangle từ ảnh 1 sang ảnh 2
+# cross dissolve ảnh
+``
